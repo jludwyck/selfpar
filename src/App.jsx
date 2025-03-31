4603732cd055c8ab6a239cb5a508af67b479d4a2
Jeff Ludwyck <jeffludwyck@Jludwyck-Macbook.local>
2025-03-30 18:04:57 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-center p-10">
+      <h1 className="text-4xl font-bold text-green-800">SelfPar</h1>
+      <p className="mt-4 text-gray-700">Master Life's Rounds</p>
+    </div>
+  )
+}
+
+export default App
